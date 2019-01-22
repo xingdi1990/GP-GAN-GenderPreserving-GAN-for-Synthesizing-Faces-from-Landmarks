c3bc10c9c5af2c5d0d6b8ca5af8b8ba7667d0f64
Xing Di (底兴) <xing.di@rutgers.edu>
2019-01-22 10:26:29 -0500

    Update download_lfw_landmark_dataset.sh

--- datasets/download_lfw_landmark_dataset.sh
+++ datasets/download_lfw_landmark_dataset.sh
@@ -1,7 +1,7 @@
 URL=https://www.dropbox.com/s/zpo35fq1vafe637/gpgan_lfw.zip?dl=0
 ZIP_FILE=./gpgan_lfw.zip
-TARGET_DIR=./lfw/
+TARGET_DIR=./
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./datasets/
+unzip $ZIP_FILE -d $TARGET_DIR
 rm $ZIP_FILE
